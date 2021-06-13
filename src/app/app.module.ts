0ada4749c12185558e02abd1a489234f6142b6b2
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2021-06-13 19:30:36 -0400

    Adding a new custom paragraph component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParagraphComponent
   ],
   imports: [
     BrowserModule,
