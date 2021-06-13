58bedd615c96dd69bf75a21084e4099035123b05
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2021-06-13 19:33:29 -0400

    Adding card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParagraphComponent } from './paragraph/paragraph.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParagraphComponent
+    ParagraphComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
